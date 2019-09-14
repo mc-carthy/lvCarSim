16976f04598040be3b3c1c92a787ceee15187367
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-09-14 13:24:37 +0100

    Normalise terrain heightmap from 0 to 1

--- src/map/terrain.lua
+++ src/map/terrain.lua
@@ -17,9 +17,14 @@ function Terrain:new(params)
         y = params.noiseOffset and params.noiseOffset.y or love.math.random()
     }
     inst.noiseScale = params.noiseScale or defaults.scale
+
+    inst.minHeight = 1
+    inst.maxHeight = 0
+
     
     inst.draw = self.draw
     inst.initialise = self.initialise
+    inst.normalise = self.normalise
     
     self.generate(inst)
     
@@ -29,6 +34,7 @@ end
 function Terrain:generate()
     self.height = {}
     self:initialise()
+    self:normalise()
 end
 
 function Terrain:draw()
@@ -47,7 +53,21 @@ function Terrain:initialise()
         self.height[x] = {}
         for y = 1, self.size.y do
             local height = love.math.noise(x * self.noiseScale + self.noiseOffset.x, y * self.noiseScale + self.noiseOffset.y)
+            if height < self.minHeight then self.minHeight = height end
+            if height > self.maxHeight then self.maxHeight = height end
             self.height[x][y] = height
+        end
+    end
+end
+
+function Terrain:normalise()
+    local range = self.maxHeight - self.minHeight
+    local multiplier = 1 / range
+    for x = 1, self.size.x do
+        for y = 1, self.size.y do
+            self.height[x][y] = (self.height[x][y] - self.minHeight) * multiplier
+            if self.height[x][y] < self.minHeight then self.minHeight = self.height[x][y] end
+            if self.height[x][y] > self.maxHeight then self.maxHeight = self.height[x][y] end
         end
     end
 end
